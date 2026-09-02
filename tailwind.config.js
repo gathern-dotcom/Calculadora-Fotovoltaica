8dc39e76b1f3fd14e70e65b82fd1cbaff3cd7360
gathern-dotcom <gathern123456@gmail.com>
2026-09-01 20:57:23 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
         'brand-success': '#149E60',
         'brand-panel': '#F5F6F7',
         border: '#DCDDE0'
+      },
+      fontFamily: {
+        sans: ['Century Gothic', 'CenturyGothic', 'AppleGothic', 'Segoe UI', 'sans-serif'],
+        mono: ['Century Gothic', 'CenturyGothic', 'AppleGothic', 'Segoe UI', 'sans-serif']
       }
     }
   },
